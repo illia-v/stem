2a64743db0eaa03a230270473ce5304c1e368c47
Illia Volochii <illia.volochii@gmail.com>
2020-04-30 19:52:42 +0300

    Move utility functions for asynchronous tests to `stem.util.test_tools`

--- stem/util/test_tools.py
+++ stem/util/test_tools.py
@@ -28,7 +28,9 @@ to match just against the prefix or suffix. For instance...
   stylistic_issues - checks for PEP8 and other stylistic issues
 """
 
+import asyncio
 import collections
+import functools
 import linecache
 import multiprocessing
 import os
@@ -599,3 +601,27 @@ def _python_files(paths):
 
       if not skip:
         yield file_path
+
+
+def async_test(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+    loop = asyncio.new_event_loop()
+    try:
+      result = loop.run_until_complete(func(*args, **kwargs))
+    finally:
+      loop.close()
+    return result
+  return wrapper
+
+
+def coro_func_returning_value(return_value):
+  async def coroutine_func(*args, **kwargs):
+    return return_value
+  return coroutine_func
+
+
+def coro_func_raising_exc(exc):
+  async def coroutine_func(*args, **kwargs):
+    raise exc
+  return coroutine_func
