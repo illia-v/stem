9529cd6ba84afdbb81d723f3df071f114f1037b1
Illia Volochii <illia.volochii@gmail.com>
2020-04-30 19:30:52 +0300

    Fix `stem.connection._msg` operating a synchronous controller

--- stem/connection.py
+++ stem/connection.py
@@ -1014,7 +1014,10 @@ async def _msg(controller, message):
     await controller.send(message)
     return await controller.recv()
   else:
-    return await controller.msg(message)
+    message = controller.msg(message)
+    if asyncio.iscoroutine(message):
+      message = await message
+    return message
 
 
 def _connection_for_default_port(address):
